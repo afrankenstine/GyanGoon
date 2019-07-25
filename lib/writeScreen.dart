71299bf18b540bed42008d23ec455ffbd51a8e8b
Yogesh Bhandari <fireygenius@gmail.com>
2019-07-25 14:27:47 +0545

    ScreenShot mechanism

--- lib/writeScreen.dart
+++ lib/writeScreen.dart
@@ -135,3 +135,29 @@ class _WriteScreenState extends State<WriteScreen> {
     );
   }
 }
+
+
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint =  Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+        print('PAinting');
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+}
