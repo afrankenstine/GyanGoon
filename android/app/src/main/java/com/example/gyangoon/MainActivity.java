d3fa5278564865298b9b1c6cfdc7151b772353fd
Yogesh Bhandari <fireygenius@gmail.com>
2019-07-25 07:30:50 +0545

    Native files added

--- android/app/src/main/java/com/example/gyangoon/MainActivity.java
+++ android/app/src/main/java/com/example/gyangoon/MainActivity.java
@@ -1,7 +1,31 @@
 package com.example.gyangoon;
 
+//package ai.fritz.tflitedemo;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.gyangoon.DigitsDetector;
+
+//import ai.fritz.tflitedemo.ml.DigitsDetector;
+//import ai.fritz.tflitedemo.ui.PaintView;+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+//import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
